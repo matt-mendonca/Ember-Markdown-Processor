de3f2a619e26dc444a9c0cae940bdabbb39edffa
Matt Mendonca <matt.mendonca1@gmail.com>
2014-05-10 13:22:11 -0400

    Update to styling

--- app/scripts/routes/index_route.js
+++ app/scripts/routes/index_route.js
@@ -2,7 +2,7 @@ EmberMarkdownProcessor.IndexRoute = Ember.Route.extend({
     
     model: function () {
         return EmberMarkdownProcessor.markdownModel.create({
-            markdown: "#h1\n##h2\np text"
+            markdown: "this is example text\n#h1\n##h2\np text"
         });
     }
 });
